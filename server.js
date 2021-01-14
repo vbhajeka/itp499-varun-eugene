59810f76a5a69b0eca8ab35498f4f3d030201cc3
Varun Bhajekar <getvaruncode@aim.com>
2021-01-14 01:37:41 -0800

    testing env vars

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ const checkJwt = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH_DOMAIN}/.well-known/jwks.json`,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
   }),
 
-  audience: process.env.AUTH_AUDIENCE,
-  issuer: `https://${process.env.AUTH_DOMAIN}/`,
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ['RS256'],
 });
 
