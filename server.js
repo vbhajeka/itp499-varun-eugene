3bd173a6beb55ddd8702238401299dbd10541f99
Varun Bhajekar <getvaruncode@aim.com>
2021-01-14 02:25:24 -0800

    testing...

--- server.js
+++ server.js
@@ -55,6 +55,27 @@ if (process.env.NODE_ENV === 'production') {
   app.use(forceSSL);
 }
 
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
+        imgSrc: ["'self'", 'data:'],
+        connectSrc: [
+          "'self'",
+          `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
+        ],
+        fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+        objectSrc: ["'self'"],
+        mediaSrc: ["'self'"],
+        frameSrc: ["'self'", `${process.env.AUTH0_DOMAIN}`],
+      },
+    },
+  })
+);
+
 app.post('/api/external', checkJwt, (req, res) => {
   console.log(req.headers.authorization);
   res.send({
